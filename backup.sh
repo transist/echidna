d0a9f1d21f4b9a3499c4dd13a11465b12df9ce7a
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-01 15:23:27 +0800

    echidna1 configuration files

---
+++ backup.sh
@@ -0,0 +1,19 @@
+FILES=`cat FILES`
+
+DESTDIR=`dirname $0`/conf
+rm -fr $DESTDIR
+for FILE in $FILES;
+do
+	if [ ! -f  "$FILE" ]; then
+		echo "file $FILE does not exist"
+		exit 1
+	fi
+	SRCDIR=`dirname $FILE` 
+	TARGETDIR=$DESTDIR/$SRCDIR
+
+	#echo "mkdir -p $TARGETDIR"
+	mkdir -p $TARGETDIR
+
+	#echo "cp $FILE $TARGETDIR"
+	cp $FILE $TARGETDIR
+done
